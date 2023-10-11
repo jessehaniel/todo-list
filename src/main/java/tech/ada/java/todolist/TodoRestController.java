28b7215af833d6fa9bf7b83c40aa8e7502c47156
jessehaniel <jesse.haniel@gmail.com>
2023-10-11 16:17:12 -0300

    Aula 2 - 9/10 - criação dos endpoints
    - POST /todo-itens
    - GET /todo-itens/{id}
    - GET /todo-itens?descricao=

--- src/main/java/tech/ada/java/todolist/TodoRestController.java
+++ src/main/java/tech/ada/java/todolist/TodoRestController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/todo-itens")
+@RequestMapping("/todo-itens")//http://localhost:8080/todo-itens/
 public class TodoRestController {
 
     //cadastrar
